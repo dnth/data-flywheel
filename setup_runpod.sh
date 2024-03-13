b9e4be46ac5c9a268010565d835e484ae511488d
dnth <dickson.neoh@gmail.com>
2024-03-13 05:32:53 +0000

    add runpod installation script

---
+++ setup_runpod.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+
+echo -e "${GREEN}Setting up conda virtual env${NC}"
+cd /root
+echo -e "${YELLOW}Downloading Miniforge installer...${NC}"
+wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge-pypy3-Linux-x86_64.sh
+echo -e "${YELLOW}Installing Miniforge...${NC}"
+bash Miniforge-pypy3-Linux-x86_64.sh -b -p /root/miniforge-pypy3
+source /root/miniforge-pypy3/bin/activate
+echo -e "${YELLOW}Creating conda environment...${NC}"
+conda create -n icevision python=3.9 -y
+conda activate icevision
+
+echo -e "${GREEN}Installing icevision from master${NC}"
+cd ..
+echo -e "${YELLOW}Cloning icevision repository...${NC}"
+git clone https://github.com/dnth/icevision
+cd icevision
+echo -e "${YELLOW}Installing icevision dependencies...${NC}"
+pip install -e .[all]
+
+echo -e "${GREEN}Installing torch and its dependencies${NC}"
+pip install torch==1.10.0+cu113 torchvision==0.11.1+cu113 torchtext==0.11.0 -f https://download.pytorch.org/whl/torch_stable.html --upgrade
+
+echo -e "${GREEN}Installing mmcv${NC}"
+pip install mmcv-full==1.3.17 -f https://download.openmmlab.com/mmcv/dist/cu113/torch1.10.0/index.html --upgrade -q
+
+echo -e "${GREEN}Installing mmdet${NC}"
+pip install mmdet==2.17.0 --upgrade -q
+
+echo -e "${GREEN}Setup complete!${NC}"
