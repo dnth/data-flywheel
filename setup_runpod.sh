05b56b4a2015f5d213990acf8d1a2497ad17cf77
dnth <dickson.neoh@gmail.com>
2024-03-18 06:09:48 +0000

    update installation sh

--- setup_runpod.sh
+++ setup_runpod.sh
@@ -29,6 +29,7 @@ pip install torch==1.13.1+cu117 torchvision==0.14.1+cu117 torchaudio==0.13.1 --e
 
 echo -e "${GREEN}Installing mmcv${NC}"
 pip install -U openmim
+pip install -U ninja psutil
 mim install mmcv-full==1.6.0
 
 echo -e "${GREEN}Installing mmdet${NC}"
