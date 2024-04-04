2ce336c2e430c11176c10cc4cebf48575ce79b47
dnth <dickson.neoh@gmail.com>
2024-04-04 08:42:52 +0000

    add pipline

---
+++ run_pipeline.py
@@ -0,0 +1,20 @@
+from data_flywheel import DataFlywheel
+
+# Create a configuration dictionary
+config = {
+    'annotation_path': '/root/data-flywheel/notebooks/objectlab/pascal_voc_annotations/',
+    'image_path': '/workspace/yolo_v8_training/oiv7_full/validation/',
+    'wandb_project': 'data-flywheel-test'
+}
+
+# Instantiate the DataFlywheel
+flywheel = DataFlywheel(config)
+
+
+flywheel.load_annotations()
+
+flywheel.train_model(batch_size=32, epoch=3)
+
+annotations_to_review = flywheel.get_most_wrong(method='top-loss')
+
+flywheel.relabel_data()
