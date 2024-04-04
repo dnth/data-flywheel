d39c9464940a3c91ae47cee7c340be2c82ec0755
dnth <dickson.neoh@gmail.com>
2024-04-04 12:50:53 +0000

    update pipeline

--- run_pipeline.py
+++ run_pipeline.py
@@ -4,6 +4,7 @@ from data_flywheel import DataFlywheel
 config = {
     'annotation_path': '/root/data-flywheel/notebooks/objectlab/pascal_voc_annotations/',
     'image_path': '/workspace/yolo_v8_training/oiv7_full/validation/',
+    'log_wandb': True,
     'wandb_project': 'data-flywheel-test'
 }
 
@@ -13,7 +14,7 @@ flywheel = DataFlywheel(config)
 
 flywheel.load_annotations()
 
-flywheel.train_model(batch_size=32, epoch=3)
+flywheel.train_model(batch_size=32, epoch=10, freeze_epoch=3)
 
 annotations_to_review = flywheel.get_most_wrong(method='top-loss')
 
