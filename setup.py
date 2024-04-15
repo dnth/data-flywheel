f217bc226eaf9318ab8bdcde90ce22b72580aeb4
dnth <dickson.neoh@gmail.com>
2024-04-15 06:58:13 +0000

    add  setup and req

--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ with open('requirements.txt') as f:
 setup(
     name='data_flywheel',
     version='0.0.1',
+    description='Iteratively correct your bounding box annotations using Tesla\'s data engine framework.',
+    author='Dickson Neoh',
+    url="https://github.com/dnth/data-flywheel",
     packages=find_packages(),
     install_requires=required,
 )
